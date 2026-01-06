6d45a283176a144926862f75f77a1354f46c73dc
HQM <nur.hakim.arif@gmail.com>
2026-01-07 02:15:52 +0700

    skip the static build

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "Building the project..."
 python3 -m pip install -r requirements.txt
-echo "Collect Static..."
-python3 manage.py collectstatic --noinput --clear
+
+# echo "Collect Static..."
+# python3 manage.py collectstatic --noinput --clear
+echo "Build complete."
