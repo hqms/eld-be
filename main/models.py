9896434d7aac34676d8b70b12db6a39e392edfb9
HQM <nur.hakim.arif@gmail.com>
2026-01-07 00:17:35 +0700

    intial commit

---
+++ main/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.dispatch import receiver
+
+# Driver model linked to Django's built-in User model
+class Driver(models.Model):
+    user = models.OneToOneField('auth.User', on_delete=models.CASCADE)
+    license_number = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.user.username
+
+@receiver(models.signals.post_save, sender='auth.User')
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Driver.objects.create(user=instance)
+    else:
+        instance.driver.save()
+
+
+class Trip(models.Model):
+    current_location = models.CharField(max_length=255)
+    pickup_location = models.CharField(max_length=255)
+    dropoff_location = models.CharField(max_length=255)
+    estimated_cycle_used = models.IntegerField()
+    driver = models.ForeignKey(Driver, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Trip from {self.pickup_location} to {self.dropoff_location}"
