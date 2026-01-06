48034a4acc202449414e79e0a5bcd374acf909a0
HQM <nur.hakim.arif@gmail.com>
2026-01-07 00:21:13 +0700

    admin interface for the Driver and Admin model

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Driver, Trip
 
-# Register your models here.
+@admin.register(Driver)
+class DriverAdmin(admin.ModelAdmin):
+    list_display = ('user', 'license_number')
+
+@admin.register(Trip)
+class TripAdmin(admin.ModelAdmin):
+    list_display = ('pickup_location', 'dropoff_location', 'driver', 'created_at')  
