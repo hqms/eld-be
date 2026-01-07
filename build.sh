bf58c99e752065f72c54823cc6fd2dd3eed2c2af
HQM <nur.hakim.arif@gmail.com>
2026-01-07 20:08:45 +0700

    add whitenoise to serve statics

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 echo "Building the project..."
 python3 -m pip install -r requirements.txt
 
-# echo "Collect Static..."
-# python3 manage.py collectstatic --noinput --clear
+echo "Collect Static..."
+python3 manage.py collectstatic --noinput --clear
 echo "Build complete."
--- elg/settings.py
+++ elg/settings.py
@@ -40,11 +40,12 @@ INSTALLED_APPS = [
     'main',
     'rest_framework',
     'drf_spectacular',
-    'rest_framework_simplejwt',
+    'rest_framework_simplejwt',    
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 REST_FRAMEWORK = {
     # YOUR SETTINGS
@@ -137,3 +139,9 @@ SPECTACULAR_SETTINGS = {
 }
 
 WSGI_APPLICATION = 'elg.wsgi.app'
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
